8b4343cc52eeb4d0b01512c320b0b0ad31277e8e
SnakeRule <jerevaltanen@gmail.com>
2016-02-21 11:59:38 +0200

    Initial commit

---
+++ BattleCity/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace BattleCity
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        Player player;
+        private DispatcherTimer dispatcherTimer;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            player = new Player(Canvas);
+
+            // Setting up the timer that runs the Game method
+            dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += Game;
+            dispatcherTimer.Interval = TimeSpan.FromTicks(TimeSpan.TicksPerSecond / 60);
+            dispatcherTimer.Start();
+        }
+
+        public void Game(object sender, object e)
+        {
+            player.drawPlayer();
+        }
+    }
+}
---
+++ BattleCity/Player.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace BattleCity
+{
+    public class Player
+    {
+        Image tankSprite;
+        protected int v { get; set; }
+
+        // Default constructor for player
+        public Player(Canvas canvas)
+        {
+            // Setting up the key presses
+            Window.Current.CoreWindow.KeyDown += onKeyDown;
+            Window.Current.CoreWindow.KeyUp += onKeyUp;
+
+            // Setting up the player sprite
+            tankSprite = new Image();
+            BitmapImage bitmapImage = new BitmapImage();
+            Uri uri = new Uri("ms-appx:///Assets/tank.png");
+            bitmapImage.UriSource = uri;
+            tankSprite.Source = bitmapImage;
+            tankSprite.Width = 400;
+            tankSprite.Height = 400;
+
+            // Setting player position on canvas
+            Canvas.SetLeft(tankSprite, 600);
+            Canvas.SetTop(tankSprite, 300);
+
+            // Adding sprite to canvas
+            canvas.Children.Add(tankSprite);
+        }
+
+
+        // Method when pressing down on a key
+        public void onKeyDown(Windows.UI.Core.CoreWindow sender, Windows.UI.Core.KeyEventArgs args)
+        {
+
+            // Moving the player
+            if (args.VirtualKey == Windows.System.VirtualKey.Left) v = 1;
+            else if (args.VirtualKey == Windows.System.VirtualKey.Right) v = 3;
+            else if (args.VirtualKey == Windows.System.VirtualKey.Up) v = 2;
+            else if (args.VirtualKey == Windows.System.VirtualKey.Down) v = 4;
+
+        }
+
+        // This is the method where the player is drawn on the screen each frame
+        public void drawPlayer()
+        {
+            switch (v)
+            {
+                case 1:
+                    {
+                        Canvas.SetLeft(tankSprite, Canvas.GetLeft(tankSprite) - 6);
+                        
+                        break;
+                    }
+                case 2:
+                    {
+                        Canvas.SetTop(tankSprite, Canvas.GetTop(tankSprite) - 6);
+                        break;
+                    }
+                case 3:
+                    {
+                        Canvas.SetLeft(tankSprite, Canvas.GetLeft(tankSprite) + 6);
+                        break;
+                    }
+                case 4:
+                    {
+                        Canvas.SetTop(tankSprite, Canvas.GetTop(tankSprite) + 6);
+                        break;
+                    }
+                case 5:
+                    {
+                        Canvas.SetLeft(tankSprite, Canvas.GetLeft(tankSprite));
+                        break;
+                    }
+                case 6:
+                    {
+                        Canvas.SetLeft(tankSprite, Canvas.GetLeft(tankSprite));
+                        break;
+                    }
+                case 7:
+                    {
+                        Canvas.SetLeft(tankSprite, Canvas.GetLeft(tankSprite));
+                        break;
+                    }
+                case 8:
+                    {
+                        Canvas.SetTop(tankSprite, Canvas.GetTop(tankSprite));
+                        break;
+                    }
+            }
+        }
+
+        // Method for releasing the keyboard press
+        private void onKeyUp(Windows.UI.Core.CoreWindow sender, Windows.UI.Core.KeyEventArgs args)
+        {
+            if (args.VirtualKey == Windows.System.VirtualKey.Left) v = 5;
+            if (args.VirtualKey == Windows.System.VirtualKey.Up) v = 6;
+            if (args.VirtualKey == Windows.System.VirtualKey.Right) v = 7;
+            if (args.VirtualKey == Windows.System.VirtualKey.Down) v = 8;
+        }
+
+
+        public Image getPlayer()
+        {
+            return tankSprite;
+        }
+    }
+}
